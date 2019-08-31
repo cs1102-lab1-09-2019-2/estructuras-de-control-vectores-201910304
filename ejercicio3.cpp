4a608b08bccd1d7a4f8d70b22663ad55372ce992
201910304 <54292881+201910304@users.noreply.github.com>
2019-08-30 19:34:43 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,30 +1,38 @@
 
-int mediana(vector<int> &lista);
+#include <vector>
+#include <iostream>
+using namespace std;
+
+float mediana(vector<int> &lista);
 vector<int> leerConsola();
 
 vector<int> leerConsola() {
-    //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
+  vector<int>lista;
+  int numero;
+  do{
+    cout<<"Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:\n";
+    cin>>numero;
+    if(numero==-1)
+      break;
+    lista.push_back(numero);
+  }while(numero!=-1);
+    return lista;
 }
 
-int mediana(vector<int> &lista) {
-    /*
-    Si el vector es 3, 9, 11, 15
-    Si el número de observaciones es par,
-     la mediana corresponde al promedio de los dos valores centrales.
-     Por ejemplo, en la muestra 3, 9, 11, 15, la mediana es (9+11)/2=10.
-     (v[4/2 -1] + v[4/2])/2 = (v[1] + v[2])/2 = (9+11)/2
-
-    Si el vector es 3, 9, 11, 15, 16
-    Si el numero de observaciones es impar,
-     Por ejemplo, en la muestra 3, 9, 11, 15, 16, la mediana es 11
-     v[4/2] = v[2] = 11
-     */
-    return null;
+float mediana(vector<int> &lista) {
+   float mediana;
+   int posicion;
+   posicion=(lista.size()/2)-1;
+   if (lista.size()%2==0)
+    mediana=(lista[posicion]+lista[posicion+1])/2.0;
+  else
+    mediana= lista[posicion+1];
+    return mediana;
 }
 
 int main() {
     cout<<"Calcular la mediana de los numeros ingresados:\n\n";
     vector<int> leer_usuario = leerConsola();
-    int mediana_elemento = mediana(leer_usuario);
+    float mediana_elemento = mediana(leer_usuario);
     cout<<"La mediana es: "<<mediana_elemento<<"\n";
 }
